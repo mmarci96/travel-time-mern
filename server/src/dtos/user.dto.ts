7d60f9a4493d3b7c48735627fa123b77c7362711
mmarci96 <sarosdimarci@gmail.com>
2024-11-19 00:53:07 +0100

    dto for serving user list

---
+++ server/src/dtos/user.dto.ts
@@ -0,0 +1,13 @@
+// No need to get crazy but I just dont want to serve the password even if its hashed.
+// Took some notes last modules 
+export interface UserDTO {
+    id: string;
+    username: string;
+    email: string;
+}
+
+export const toUserDTO = (user: any): UserDTO => ({
+    id: user._id.toString(), // Convert ObjectId to string
+    username: user.username,
+    email: user.email,
+});
