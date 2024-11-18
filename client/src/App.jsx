d0c0c5c8a85277340266ec888404fcb0a0cbeed3
mmarci96 <sarosdimarci@gmail.com>
2024-11-18 14:51:39 +0100

    server and client initialiyed

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./scenes/Home"
+import PageNotFound from "./scenes/PageNotFound"
+
+
+function App() {
+
+  return (
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<PageNotFound />} />
+        </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
