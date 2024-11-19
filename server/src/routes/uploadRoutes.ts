9e77bbb98e7299d1cd94c8019828232d7bd21b59
mmarci96 <sarosdimarci@gmail.com>
2024-11-19 10:48:53 +0100

    commit before some refactoring logic into a service layer

--- server/src/routes/uploadRoutes.ts
+++ server/src/routes/uploadRoutes.ts
@@ -20,14 +20,26 @@ const storage = multer.diskStorage({
 const imageUpload = multer({storage: storage})
 
 // I guess the argument we send in will handle the upload?
-router.post('/img', imageUpload.array("image-file"), async (req, res, next) => {
-    try{
-        console.log('img post request recieved!');
-        console.log('Axios POST body: ', req.body);
-        res.send({'status':'uploaded'});
-    }catch(err){
-        next(err)
+router.post(
+  '/img',
+  imageUpload.array("image-file"),
+  async (req: Request, res: Response, next: NextFunction): Promise<any> => {
+    if (!req.files) {
+      return res.status(400).send("No file uploaded.");
     }
-});
+    try {
+      // Type assertion for req.files
+      const files = req.files as Express.Multer.File[];
+
+      // Extract filenames
+      const fileNames = files.map(file => ({name: file.filename, path: file.path}));
+
+      // Send response with file names
+      res.status(201).json({ status: 'uploaded', name: fileNames[0].name, path: fileNames[0].path });
+    } catch (err) {
+      next(err);
+    }
+  }
+);
 
 export default router;
