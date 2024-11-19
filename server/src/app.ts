bddb6bb43852494e3490b43fc7a3e6da8c450e37
mmarci96 <sarosdimarci@gmail.com>
2024-11-19 09:49:03 +0100

    added abs path for now inside the upload route and added img folder, committing before adding it to gitignore

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import multer from 'multer'
+import multer from 'multer';
 import bodyParser from 'body-parser';
 import errorHandler from './middleware/errorHandler';
 import authRoutes from './routes/authRoutes';
@@ -13,17 +13,16 @@ dotenv.config();
 const app = express();
 const { MONGO_URL, PORT = 8080 } = process.env;
 
-
 app.use(express.json());
 app.use(errorHandler);
 
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
-app.use('/api/upload', uploadRoutes)
+app.use('/api/upload', uploadRoutes);
 
 const main = async () => {
     const url = MONGO_URL;
-    if(!url){
+    if (!url) {
         console.error('Missing MONGO_URL environment variable');
         process.exit(1);
     }
--- server/src/routes/uploadRoutes.ts
+++ server/src/routes/uploadRoutes.ts
@@ -1,9 +1,11 @@
 import express, { Request, Response, NextFunction } from 'express';
 
 const router = express.Router();
+// TODO: Its better to use absolute path for this include it in .env
+const imageUploadPath = '/home/meirl/Documents/travel-time-mern/server/img_uploads_temp';
 
-router.post('/img', async (req,res,next) => {
-    console.log('img post request recieved!')
-})
+router.post('/img', async (req, res, next) => {
+    console.log('img post request recieved!');
+});
 
-export default router
+export default router;
