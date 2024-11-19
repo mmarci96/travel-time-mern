08d9284dc21f636a5468adbc61858879f56bc06d
mmarci96 <sarosdimarci@gmail.com>
2024-11-19 09:30:58 +0100

    img upload testing seems fine on client side

--- client/src/components/ImageUpload.jsx
+++ client/src/components/ImageUpload.jsx
@@ -22,7 +22,7 @@ const ImageUpload = () => {
     setLoading(true);
     setError(null);
     try {
-      const res = await axios.post('/api/upload', formData, {
+      const res = await axios.post('/api/upload/img', formData, {
         headers: {
           'Content-Type': 'multipart/formdata',
         },
