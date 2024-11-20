133cc6543ca3e4612ec9e1d431299abee41a1940
Ditroi_Toth_Zsuzsa <157112767+DTZsuzsi@users.noreply.github.com>
2024-11-20 11:50:41 +0100

    userdetails snake cas corrected

--- server/src/model/UserDetailsModel.ts
+++ server/src/model/UserDetailsModel.ts
@@ -2,19 +2,19 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const userDetailsSchema = new Schema({
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
+    first_name: { type: String, required: true },
+    last_name: { type: String, required: true },
     birthdate: { type: Date, required: true },
     bio: { type: String, default: '' },
     location: { type: String, default: '' },
     interests: { type: Array, default: [] },
-    visitingList: { type: Array, default: [] },
+    visiting_list: { type: Array, default: [] },
     gender: { type: String, default: null },
-    socialMediaLinks: { platform: String, link: String, default: {} },
-    languagesSpoken: { type: Array, default: [] },
-    avatarUrl: { type: String },
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date },
+    social_media_links: { platform: String, link: String, default: {} },
+    languages_spoken: { type: Array, default: [] },
+    avatar_url: { type: String },
+    created_at: { type: Date, default: Date.now },
+    updated_at: { type: Date },
 });
 
 const UserDetailsModel = mongoose.model('UserDetails', userDetailsSchema);
