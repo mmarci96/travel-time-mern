09b9f050f5c418f431fea3eab4deb961d53b223e
Ditroi_Toth_Zsuzsa <157112767+DTZsuzsi@users.noreply.github.com>
2024-11-20 11:49:10 +0100

    snake case corrected in UserModel

--- server/src/model/UserModel.ts
+++ server/src/model/UserModel.ts
@@ -5,9 +5,9 @@ const userSchema = new Schema({
     username: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date },
-    userDetails: { type: Schema.Types.ObjectId, ref: 'UserDetails' },
+    created_at: { type: Date, default: Date.now },
+    updated_at: { type: Date },
+    user_details: { type: Schema.Types.ObjectId, ref: 'UserDetails' },
 });
 
 const UserModel = mongoose.model('User', userSchema);
