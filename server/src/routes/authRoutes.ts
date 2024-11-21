185165fa37a723ca95e64dc544eb74e655ac2b23
mmarci96 <sarosdimarci@gmail.com>
2024-11-21 15:08:13 +0100

    errors handled on servive layer and the createuser alreadt returns a user obj withpout pw, we might return tokens or rather just a success msg

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -1,7 +1,6 @@
 import express, { Request, Response, NextFunction } from 'express';
 import { createUser } from '../services/userService';
 import { createToken, createRefreshToken } from '../services/authService';
-import { toUserDTO } from '../dto/user.dto';
 
 const router = express.Router();
 
@@ -10,27 +9,10 @@ router.post(
     async (req: Request, res: Response, next: NextFunction): Promise<any> => {
         try {
             const { username, email, password } = req.body;
-            if (!username || !email || !password) {
-                return res
-                    .status(400)
-                    .json({ error: 'All fields are required' });
-            }
+            const user = await createUser(username, email, password);
 
-            const newUser = await createUser(username, email, password);
-            // const userDTO = toUserDTO(newUser); // Use the DTO to sanitize the response
-            const response = {
-                username: newUser.username,
-                email: newUser.email,
-            };
-
-            return res.status(201).send(response);
+            return res.status(201).send(user);
         } catch (err: any) {
-            if (
-                err.message === 'Username is already taken' ||
-                err.message === 'Email is already registered'
-            ) {
-                return res.status(409).json({ error: err.message });
-            }
             next(err);
         }
     },
@@ -41,23 +23,13 @@ router.post(
     async (req: Request, res: Response, next: NextFunction): Promise<any> => {
         try {
             const { email, password } = req.body;
-            if (!email || !password) {
-                return res
-                    .status(400)
-                    .json({ error: 'All fields are required' });
-            }
+
             const newToken = await createToken(email, password);
             const refreshToken = await createRefreshToken(email, password);
-            console.log(newToken, refreshToken);
+
             const response = { token: newToken, refreshToken: refreshToken };
             return res.status(200).send(response);
         } catch (err: any) {
-            if (
-                err.message === 'No user with email' ||
-                err.message === 'Wrong password!'
-            ) {
-                return res.status(409).json({ error: err.message });
-            }
             next(err);
         }
     },
