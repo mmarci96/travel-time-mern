4dc1b1dbd475c364191b5b288ba5fd9ad63ff0d1
mmarci96 <sarosdimarci@gmail.com>
2024-11-26 02:08:34 +0100

    made custom hook for uploading image, added a test endpoint to client and adjusted changes from backend

--- client/src/components/posts/PostList.jsx
+++ client/src/components/posts/PostList.jsx
@@ -3,23 +3,18 @@ import { useEffect, useState } from 'react';
 
 const PostList = () => {
   const [posts, setPosts] = useState([]);
-  const { loading, error, sendRequest } = useAuthRequest(
-    '/api/posts',
-    'GET',
-  );
+  const { loading, error, sendRequest } = useAuthRequest('/api/posts', 'GET');
   useEffect(() => {
-    sendRequest().then(data => data ? setPosts(data) : setPosts([]));
+    sendRequest().then((data) => (data ? setPosts(data) : setPosts([])));
   }, []);
 
-
   return (
-    <ul>PostList
-      {posts.length > 0 && posts.map((post) => (
-        <li key={post._id}>
-          {post.title}
-        </li>
-      ))}
-    </ul>);
+    <ul>
+      PostList
+      {posts.length > 0 &&
+        posts.map((post) => <li key={post._id}>{post.title}</li>)}
+    </ul>
+  );
 };
 
 export default PostList;
