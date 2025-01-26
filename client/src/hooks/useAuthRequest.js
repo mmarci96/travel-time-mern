bd750bce164e9a12d75c01eeeb8868e42746ff62
mmarci96 <sarosdimarci@gmail.com>
2025-01-26 19:47:19 +0100

    a simple yet easy way to refresh expired token

--- client/src/hooks/useAuthRequest.js
+++ client/src/hooks/useAuthRequest.js
@@ -4,6 +4,24 @@ export const useAuthRequest = () => {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
 
+    const handleRefreshToken = async () => {
+        const refreshToken = localStorage.getItem('refresh_token')
+        const res = await fetch('/api/auth/refresh_token', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${refreshToken}`,
+            },
+        })
+        const data = await res.json();
+        if(res.ok && data){
+            localStorage.setItem("token", data.token)
+            return data.token 
+        }
+
+        return null;
+    }
+
     const sendRequest = async (endpoint, method, formData = null) => {
         setLoading(true);
         setError(null);
@@ -31,6 +49,12 @@ export const useAuthRequest = () => {
 
             const res = await fetch(endpoint, options);
             const data = await res.json();
+            if(data.error === 'Unauthorized'){
+                const newToken = await handleRefreshToken();
+                if(newToken){
+                    window.location.reload()
+                }
+            }
             if (!res.ok) {
                 setError(data.error.message);
                 return;
