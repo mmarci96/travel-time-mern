14d38646c4be5213f6278f92d9a2717407239584
mmarci96 <sarosdimarci@gmail.com>
2025-01-27 17:29:24 +0100

    almost the same as the schema model but added the actor name to it

---
+++ server/src/dto/notification.dto.ts
@@ -0,0 +1,31 @@
+import { Types, Date } from 'mongoose'
+import { INotification, NotificationType, TargetType } from '../model/NotificationModel';
+
+export interface NotificationDTO {
+    id: Types.ObjectId;
+    userId: Types.ObjectId;
+    type: NotificationType;
+    actorId: Types.ObjectId;
+    actorUsername: string;
+    targetId?: Types.ObjectId;
+    targetType?: TargetType;
+    message?: string;
+    read: boolean;
+    createdAt: Date;
+} 
+
+export const toNotificationDto = (
+    notification: INotification,
+    actorUsername: string
+): NotificationDTO => ({
+        id: notification._id,
+        actorUsername: actorUsername,
+        userId: notification.userId,
+        type: notification.type,
+        actorId: notification.actorId,
+        targetId: notification.targetId,
+        targetType: notification.targetType,
+        message: notification.message,
+        read: notification.read,
+        createdAt: notification.createdAt
+    })
