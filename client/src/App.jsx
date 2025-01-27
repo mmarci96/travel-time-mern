af6ab3b026d784f12673c5ebbabb9b1d9cc4ebef
mmarci96 <sarosdimarci@gmail.com>
2025-01-27 01:56:12 +0100

    lazy load all components and added the LoadAnimation as suspense

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,37 +1,43 @@
+
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './scenes/Home';
-import PageNotFound from './scenes/PageNotFound';
-import Feed from './scenes/Feed';
-import TesterScene from './scenes/TesterScene.jsx';
-import Post from './scenes/Post.jsx';
+import { lazy, Suspense } from 'react';
 import TopNavBar from './components/navigation/TopNavBar.jsx';
 import BotNavBar from './components/navigation/BotNavBar.jsx';
-import Create from './scenes/Create.jsx';
-import MyAccount from './scenes/MyAccount.jsx';
-import Notifications from './scenes/Notifications.jsx';
-import Discover from './scenes/Discover.jsx';
-import Profile from './scenes/Profile.jsx';
+import LoadAnimation from './components/common/LoadAnimation.jsx';
+
+const Home = lazy(() => import('./scenes/Home'));
+const PageNotFound = lazy(() => import('./scenes/PageNotFound'));
+const Feed = lazy(() => import('./scenes/Feed'));
+const TesterScene = lazy(() => import('./scenes/TesterScene.jsx'));
+const Post = lazy(() => import('./scenes/Post.jsx'));
+const Create = lazy(() => import('./scenes/Create.jsx'));
+const MyAccount = lazy(() => import('./scenes/MyAccount.jsx'));
+const Notifications = lazy(() => import('./scenes/Notifications.jsx'));
+const Discover = lazy(() => import('./scenes/Discover.jsx'));
+const Profile = lazy(() => import('./scenes/Profile.jsx'));
 
 function App() {
     return (
         <BrowserRouter>
             <TopNavBar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/feed" element={<Feed />} />
-                <Route path="/create" element={<Create />} />
-                <Route path="/my-account" element={<MyAccount />} />
-                <Route path="/notifications" element={<Notifications />} />
-                <Route path="/discover" element={<Discover />} />
-
-                <Route path="/profile/:userId" element={<Profile />} />
-                <Route path="/post/:postId" element={<Post />} />
-                <Route path="*" element={<PageNotFound />} />
-                <Route path="/test" element={<TesterScene />} />
-            </Routes>
+            <Suspense fallback={<LoadAnimation />}>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/feed" element={<Feed />} />
+                    <Route path="/create" element={<Create />} />
+                    <Route path="/my-account" element={<MyAccount />} />
+                    <Route path="/notifications" element={<Notifications />} />
+                    <Route path="/discover" element={<Discover />} />
+                    <Route path="/profile/:userId" element={<Profile />} />
+                    <Route path="/post/:postId" element={<Post />} />
+                    <Route path="*" element={<PageNotFound />} />
+                    <Route path="/test" element={<TesterScene />} />
+                </Routes>
+            </Suspense>
             <BotNavBar />
         </BrowserRouter>
     );
 }
 
 export default App;
+
