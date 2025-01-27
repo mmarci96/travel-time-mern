6771e713c9f885f8f2ea79e06449b213f1f3c878
mmarci96 <sarosdimarci@gmail.com>
2025-01-27 14:12:15 +0100

    Base setup for notification

--- server/src/model/NotificationModel.ts
+++ server/src/model/NotificationModel.ts
@@ -1,42 +1,52 @@
-import mongoose, { Date, Document, Schema } from "mongoose";
+import mongoose, { Date, Document, Schema } from 'mongoose';
 
 enum NotificationType {
-    FOLLOW = "FOLLOW",
-    LIKE = "LIKE",
-    COMMENT = "COMMENT",
+    FOLLOW = 'FOLLOW',
+    LIKE = 'LIKE',
+    COMMENT = 'COMMENT',
 }
 
 enum TargetType {
-    POST = "POST",
-    COMMENT = "COMMENT",
-    USER = "USER",
+    POST = 'POST',
+    COMMENT = 'COMMENT',
+    USER = 'USER',
 }
 
 interface INotification extends Document {
     _id: Schema.Types.ObjectId;
     userId: Schema.Types.ObjectId;
-    type: NotificationType; 
+    type: NotificationType;
     actorId: Schema.Types.ObjectId;
     targetId?: Schema.Types.ObjectId;
-    targetType?: TargetType; 
-    message?: string; 
-    metadata?: Record<string, any>; 
+    targetType?: TargetType;
+    message?: string;
+    metadata?: Record<string, any>;
     read: boolean;
     createdAt: Date;
 }
 
 const notificationSchema = new mongoose.Schema({
     userId: { type: Schema.Types.ObjectId, required: true, ref: 'User' },
-    type: { type: String, enum: Object.values(NotificationType), required: true }, 
+    type: {
+        type: String,
+        enum: Object.values(NotificationType),
+        required: true,
+    },
     actorId: { type: Schema.Types.ObjectId, required: true, ref: 'User' },
-    targetId: { type: Schema.Types.ObjectId, required: false }, 
-    targetType: { type: String, enum: Object.values(TargetType), required: false },  
-    message: { type: String, required: false }, 
-    metadata: { type: Schema.Types.Mixed, required: false }, 
+    targetId: { type: Schema.Types.ObjectId, required: false },
+    targetType: {
+        type: String,
+        enum: Object.values(TargetType),
+        required: false,
+    },
+    message: { type: String, required: false },
+    metadata: { type: Schema.Types.Mixed, required: false },
     read: { type: Boolean, default: false },
     createdAt: { type: Date, default: Date.now },
 });
 
-const NotificationModel = mongoose.model<INotification>("Notification", notificationSchema);
+const NotificationModel = mongoose.model<INotification>(
+    'Notification',
+    notificationSchema,
+);
 export { NotificationModel, INotification, NotificationType, TargetType };
-
--- server/src/services/notificationService.ts
+++ server/src/services/notificationService.ts
@@ -1,38 +1,39 @@
-import { Types } from 'mongoose';
+import { Types, Schema } from 'mongoose';
 import BadRequestError from '../errors/BadRequestError';
-import { NotificationModel, NotificationType, TargetType } from '../model/NotificationModel';
-
+import {
+    NotificationModel,
+    NotificationType,
+    TargetType,
+} from '../model/NotificationModel';
 
 export const getNotifications = async (userId: Types.ObjectId) => {
-
     if (!userId) {
         throw new BadRequestError({
             code: 404,
-            message: "No userid provided",
+            message: 'No userid provided',
             logging: true,
         });
     }
 
-    const notifications = await NotificationModel.find({userId: userId})
-    if(!notifications){
+    const notifications = await NotificationModel.find({ userId: userId });
+    if (!notifications) {
         throw new BadRequestError({
             code: 404,
-            message: "No new notifications",
-            logging: false
-        })
+            message: 'No new notifications',
+            logging: false,
+        });
     }
     return notifications;
-}
+};
 
 export const createNotification = async (
-    userId: Types.ObjectId, 
+    userId: Schema.Types.ObjectId,
     actorId: Types.ObjectId,
     type: NotificationType,
     targetId: Types.ObjectId,
     targetType: TargetType,
     message: string,
 ) => {
-
     const newNotification = new NotificationModel({
         userId,
         type,
@@ -44,33 +45,34 @@ export const createNotification = async (
     });
 
     await newNotification.save();
-    console.log("Notification saved:", newNotification);
+    console.log('Notification saved:', newNotification);
 };
 
 export const markRead = async (notificationId: Types.ObjectId) => {
-    if(!notificationId){
+    if (!notificationId) {
         throw new BadRequestError({
             code: 404,
-            message: "No id provided",
+            message: 'No id provided',
             logging: false,
-        })
+        });
     }
-    const notification = await NotificationModel.findOneAndUpdate({_id: notificationId}, { read: true })
-    if(!notification){
+    const notification = await NotificationModel.findOneAndUpdate(
+        { _id: notificationId },
+        { read: true },
+    );
+    if (!notification) {
         throw new BadRequestError({
             code: 400,
-            message: "Did not found notification",
+            message: 'Did not found notification',
             logging: true,
-        })
+        });
     }
 
     return notification;
-}
+};
 
 export const deleteNotification = async (notificationId: Types.ObjectId) => {
-    await NotificationModel.findByIdAndDelete(notificationId)
+    await NotificationModel.findByIdAndDelete(notificationId);
 
-    return { success: true }
-}
-
-
+    return { success: true };
+};
