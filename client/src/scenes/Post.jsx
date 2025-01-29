8e4be0da92eaa2f1ac6250e5669b2ac3b29adc72
mmarci96 <sarosdimarci@gmail.com>
2025-01-29 05:56:14 +0100

    moved the create post to the postlist component from here

--- client/src/scenes/Post.jsx
+++ client/src/scenes/Post.jsx
@@ -1,22 +1,18 @@
 import { useParams } from 'react-router-dom';
 import PostDetails from '../components/posts/PostDetails.jsx';
 import CommentList from '../components/comments/CommentList.jsx';
-import CreateComment from '../components/comments/CreateComment.jsx';
-import { useState } from 'react';
 import useAuthContext from '../hooks/useAuthContext.js';
 import LoginAlert from '../components/common/LoginAlert.jsx';
 
 const Post = () => {
-    const [comment, setComment] = useState(null);
     const { postId } = useParams();
     const { isAuthenticated } = useAuthContext();
 
     return (
         isAuthenticated ?
-        <div>
+        <div className='flex flex-wrap justify-center m-auto min-h-[80vh]'>
             <PostDetails postId={postId} />
-            <CommentList postId={postId} refresh={comment} />
-            <CreateComment postId={postId} onComment={setComment} />
+            <CommentList postId={postId} />
         </div> : <LoginAlert />
     );
 };
