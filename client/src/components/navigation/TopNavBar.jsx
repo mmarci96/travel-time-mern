8fa2efc6d7efd360a61f6e130e5ca88b4d4e9628
mmarci96 <sarosdimarci@gmail.com>
2025-01-30 21:15:56 +0100

    since navbar is on bottom sticky is not a good solution here

--- client/src/components/navigation/TopNavBar.jsx
+++ client/src/components/navigation/TopNavBar.jsx
@@ -8,7 +8,7 @@ const TopNavBar = () => {
     const { user, isAuthenticated } = useContext(AuthContext);
 
     return (
-        <div className="bg-blue-200 w-full h-12 flex z-10 sticky top-0">
+        <div className="bg-blue-200 w-full h-12 flex z-10 top-0">
             <img src="/travel-logo.png" className="h-10 mx-2 my-auto" alt="logo" />
             <h1 className="text-white bg-cyan-600 bg-opacity-80 p-1 pt-2 px-4 text-xl ml-2 m-auto tracking-wide rounded-2xl font-semibold">
                 <Link to="/feed">TravelTime</Link>
