5beb84e92a247456d0c0dca17c97a610453e5f4d
Ditroi_Toth_Zsuzsa <157112767+DTZsuzsi@users.noreply.github.com>
2025-02-26 13:44:33 +0100

    back to countries button

--- client/src/scenes/CountryDetail.jsx
+++ client/src/scenes/CountryDetail.jsx
@@ -2,7 +2,8 @@ import { useEffect, useState } from 'react';
 import useAuthContext from '../hooks/useAuthContext.js';
 import useAuthRequest from '../hooks/useAuthRequest.js';
 import CountryDetailsCard from '../components/countries/CountryDetailsCard.jsx';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import Button from '../components/common/Button.jsx';
 
 function CountryDetail() {
   const [country, setCountry] =useState(null);
@@ -23,10 +24,17 @@ console.log(url);
 
   return (
 
-      <div >
-        <p> hi </p>
+    <div className="flex flex-col items-center">
+      <div className="mt-4">
+        <Link to="/countries">
+          <Button >Back to countries</Button>
+        </Link>
+      </div>
+      <div className="flex justify-center items-center mt-8">
         <CountryDetailsCard country={country} />
       </div>
+    </div>
+
 
   )
 }
