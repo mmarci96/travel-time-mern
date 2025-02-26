7dae643fa679127fd0a9898b38b3961679197da2
Ditroi_Toth_Zsuzsa <157112767+DTZsuzsi@users.noreply.github.com>
2025-02-26 14:09:22 +0100

    country details css

--- client/src/components/countries/CountryDetailsCard.jsx
+++ client/src/components/countries/CountryDetailsCard.jsx
@@ -6,40 +6,41 @@ function CountryDetailsCard({ country }) {
   return (
     <AnimatedComponent
       children={
-        <div className="my-4 p-1 ring-1 rounded-xl max-h-[640px] h-[80vh] shadow-slate-400 shadow-md min-w-[320px] w-[60vw] max-w-[480px] mx-auto">
-
-            <ImageWithPlaceholder
-              alt={country?.name}
-              image_url={"https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQ3G9orHhCXgQphuvFN5MTfiYFPt1meQdOBLprQLlUIXH9-ehOwGyOcSW9ElNFMDgVvz1KuyLk&s=19"}
-            />
-
-          <span className="flex flex-col">
-                        <h3 className="country-name text-lg px-2  mb-1 max-h-8 overflow-hidden text-ellipsis">
-                           Country: {country?.name}
-                        </h3>
-             <h3 className="country-capital text-lg px-2  mb-1 max-h-8 overflow-hidden text-ellipsis">
-                           Capital: {country?.capital}
-                        </h3>
-             <h3 className="country-continent text-lg px-2  mb-1 max-h-8 overflow-hidden text-ellipsis">
-                           Continent: {country?.continent}
-                        </h3>
-             <h3 className="country-population text-lg px-2  mb-1 max-h-8 overflow-hidden text-ellipsis">
-                           Population: {country?.population}
-                        </h3>
-             <h3 className="country-timezones text-lg px-2  mb-1 max-h-8 overflow-hidden text-ellipsis">
-                           Timezones: {country?.timezones}
-                        </h3>
-             <h3 className="country-startOfWeek text-lg px-2  mb-1 max-h-8 overflow-hidden text-ellipsis">
-                           Start of week: {country?.start_of_week}
-                        </h3>
-                    </span>
-
-
+        <div className="my-4 p-1 ring-1 rounded-xl shadow-slate-400 shadow-md w-full max-w-2xl mx-auto">
+          <div className="flex flex-wrap md:flex-nowrap">
+            <div className="w-full md:w-1/2">
+              <ImageWithPlaceholder
+                alt={country?.name}
+                image_url={"https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQ3G9orHhCXgQphuvFN5MTfiYFPt1meQdOBLprQLlUIXH9-ehOwGyOcSW9ElNFMDgVvz1KuyLk&s=19"}
+                className="w-full h-auto object-cover rounded-l-xl"
+              />
+            </div>
+            <div className="w-full ml-2 md:w-1/2 p-4">
+              <h3 className="text-lg font-bold mb-2 max-h-8 overflow-hidden text-ellipsis">
+                Country: {country?.name}
+              </h3>
+              <h3 className="text-lg mb-2 max-h-8 overflow-hidden text-ellipsis">
+                Capital: {country?.capital}
+              </h3>
+              <h3 className="text-lg mb-2 max-h-8 overflow-hidden text-ellipsis">
+                Continent: {country?.continent}
+              </h3>
+              <h3 className="text-lg mb-2 max-h-8 overflow-hidden text-ellipsis">
+                Population: {country?.population}
+              </h3>
+              <h3 className="text-lg mb-2 max-h-8 overflow-hidden text-ellipsis">
+                Timezones: {country?.timezones}
+              </h3>
+              <h3 className="text-lg mb-2 max-h-8 overflow-hidden text-ellipsis">
+                Start of week: {country?.start_of_week}
+              </h3>
+            </div>
+          </div>
           <LoadAnimation />
-          )
         </div>
       }
     />
+
   )
 }
 
