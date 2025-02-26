6310d0717be2793e922385587d73b41f7440c8c2
Ditroi_Toth_Zsuzsa <157112767+DTZsuzsi@users.noreply.github.com>
2025-02-26 14:45:36 +0100

    sorting frontend ready

--- client/src/components/countries/CountryList.jsx
+++ client/src/components/countries/CountryList.jsx
@@ -1,44 +1,58 @@
 import React, { useEffect, useState } from 'react';
 import useAuthContext from '../../hooks/useAuthContext.js';
 import useAuthRequest from '../../hooks/useAuthRequest.js';
-import PostControls from '../posts/PostControls.jsx';
-import PostCard from '../posts/PostCard.jsx';
 import LoadAnimation from '../common/LoadAnimation.jsx';
-import Button from '../common/Button.jsx';
 import CountryCardLittle from './CountryCardLittle.jsx';
+import PostControls from '../posts/PostControls.jsx';
+import Button from '../common/Button.jsx';
+import CountryControls from './CountryControls.jsx';
 
 function CountryList() {
   const [countries, setCountries] = useState([]);
-  //const [search, setSearch] = useState('');
-  //const [limit, setLimit] = useState(8);
-  // const [sort, setSort] = useState('created_at');
-  //const [asc, setAsc] = useState(true);
-  //const [showControls, setShowControls] = useState(false);
+  const [search, setSearch] = useState('');
+  const [limit, setLimit] = useState(8);
+   const [sort, setSort] = useState('created_at');
+   const [asc, setAsc] = useState(true);
+  const [showControls, setShowControls] = useState(false);
   const { isAuthenticated } = useAuthContext();
 
   const { sendRequest } = useAuthRequest();
 
   useEffect(() => {
-    let url = `/api/countries/all`
-
-    // ?page=${1}&limit=${limit}&search=${search}&sort=${sort}&asc=${asc}`;
+    let url = `/api/countries/all?page=${1}&limit=${limit}&search=${search}&sort=${sort}&asc=${asc}`;
 
     sendRequest(url, 'GET').then((data) =>
       data ? setCountries(data.countries) : setCountries([]),
     );
-  }, [isAuthenticated]);
+  }, [limit, sort, asc, search,isAuthenticated]);
 
-  // limit, sort, asc, search,
+  const toggleSortOrder = () => {
+    setAsc(!asc);
+  };
 
-  //const toggleSortOrder = () => {
-  //  setAsc(!asc);
-  //};
-
-  //const handleNextPage = () => setLimit((prev) => prev + 5);
-  //const toggleControls = () => setShowControls(!showControls);
+  const handleNextPage = () => setLimit((prev) => prev + 5);
+  const toggleControls = () => setShowControls(!showControls);
 
   return (
-
+    <div className="flex flex-col items-center">
+      {showControls ? (
+        <CountryControls
+          search={search}
+          setSearch={setSearch}
+          sort={sort}
+          setSort={setSort}
+          asc={asc}
+          toggleSortOrder={toggleSortOrder}
+          toggleControls={toggleControls}
+        />
+      ) : (
+        <button
+          onClick={toggleControls}
+          className="p-1 px-2 bg-cyan-500 text-white hover:bg-cyan-600 transition duration-300 ease-in-out mx-auto mt-4 rounded-xl"
+        >
+          Show controls
+        </button>
+      )}
     <div className="container mx-auto p-4">
       <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
         {countries?.length > 0 ? (
@@ -53,14 +67,10 @@ function CountryList() {
       </ul>
     </div>
 
-
-  )
-
-
-  //  <div className="pagination-controls">
-  //  <Button onClick={handleNextPage} children="More" />
-  //</div>
-  // </div>
+    <div className="pagination-controls">
+    <Button onClick={handleNextPage} children="More" />
+  </div>
+   </div>)
 
 }
 
