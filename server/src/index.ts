78a4ebf0e4354b8598d4bde92bd0028a84d70d3c
mmarci96 <sarosdimarci@gmail.com>
2025-03-11 19:51:47 +0100

    minor fix with env var loading in docker compose, but now new entrypont good as old

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,8 +7,7 @@ const { MONGO_URI, PORT, HOST } = config;
 const main = async () => {
     try {
         await mongoose.connect(MONGO_URI);
-        const db = MONGO_URI.split('/')[MONGO_URI.split('/').length - 1];
-        console.log('Connected to mongo db: ' + db);
+        console.log('Connected to mongo db');
 
         app.listen(Number(PORT), HOST, () => {
             console.log(`Server is running on http://${HOST}:${PORT}`);
